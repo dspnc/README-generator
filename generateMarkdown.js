20d35d4a4f0a0009ad638cb772a8b5494f7a7b0c
dspnc <dspence.hill@gmail.com>
2023-03-22 20:18:33 -0400

    added require and basic template for inquirer prompt with questions and license badges

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,6 +1,17 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  switch(license) {
+    case 'MIT':
+        return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
+    case 'GNU GPL v3':
+        return `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`
+    case 'The Unlicense':
+        return `[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)`
+    default: 
+        return '';
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
--- index.js
+++ index.js
@@ -1,13 +1,77 @@
 // TODO: Include packages needed for this application
+const fs = require('fs')
+const inquirer = require('inquirer')
+const generateMarkdown = require('./generateMarkdown.js')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+
+//Prompt for Title, Description, Installation, Usage, Contributing, and Tests
+// +
+// License selection from list
+// 	add badge at top and notice in License section
+
+// Prompt for GitHub username and email
+// 	add to Questions section as links
+
+// Write md file with each section, including Table of Contents with clickable links to each section
+const questions = [
+    {
+        type: 'input',
+        message: 'What is the title of the project? ',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'Please provide a description of the project: ',
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'Please provide installation instructions: ',
+        name: 'installation',
+    },
+    {
+        type: 'input',
+        message: 'Please provide usage information for the project: ',
+        name: 'usage',
+    },
+    {
+        type: 'input',
+        message: 'Please describe contribution guidelines for this project: ',
+        name: 'contributing',
+    },
+    {
+        type: 'input',
+        message: 'Please provide instructions for testing: ',
+        name: 'tests',
+    },
+    {
+        type: 'list',
+        message: 'Please describe contribution guidelines for this project: ',
+        name: 'license',
+        choices: ['MIT', 'GNU GPL v3', 'The Unlicense', 'None']
+    },
+  ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+
+    fs.writeFile('README.md', , 
+    //(err) => err ? console.error(err) : console.log('Success!')
+    );
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+
+    inquirer
+  .prompt(questions)
+  .then((response) =>
+    response.confirm === response.password
+      ? console.log('Success!')
+      : console.log('You forgot your password already?!')
+  );
+}
 
 // Function call to initialize app
 init();
